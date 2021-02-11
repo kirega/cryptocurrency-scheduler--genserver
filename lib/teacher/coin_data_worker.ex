1a762d3027c961e5af138f35442c4362cfae1e1d
kirega <kirega@mesozi.com>
2021-02-11 19:24:39 +0300

    step up complete multi access pattern

--- lib/teacher/coin_data_worker.ex
+++ lib/teacher/coin_data_worker.ex
@@ -2,7 +2,8 @@ defmodule Teacher.CoinDataWorker do
   use GenServer
 
   def start_link(args) do
-    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+    id = Map.get(args, :id)
+    GenServer.start_link(__MODULE__, args, name: id)
   end
 
   def init(state) do
@@ -11,19 +12,30 @@ defmodule Teacher.CoinDataWorker do
   end
 
   def handle_info(:coin_fetch, state) do
-    price = coin_price()
-    IO.inspect("current bitcoin price is #{price}")
+    updated_state =
+      state
+      |> Map.get(:id)
+      |> coin_data()
+      |> update_state(state)
+
+    if updated_state[:price] != state[:price] do
+      IO.inspect("current #{updated_state[:name]} price is #{updated_state[:price]}")
+    end
+
     schedule_coin_fetch()
-    {:noreply, Map.put(state, :btc, price)}
+    {:noreply, updated_state}
   end
 
-  defp coin_price do
-    "https://api.coincap.io/v2/assets/bitcoin"
+  defp update_state(%{"id" => name, "priceUsd" => price}, existing_state) do
+    Map.merge(existing_state, %{name: name, price: price})
+  end
+
+  defp coin_data(id) do
+    "https://api.coincap.io/v2/assets/#{id}"
     |> HTTPoison.get!()
     |> Map.get(:body)
     |> Jason.decode!()
     |> Map.get("data")
-    |> Map.get("priceUsd")
   end
 
   defp schedule_coin_fetch() do
